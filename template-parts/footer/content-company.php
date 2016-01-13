40d69159972241038366f2a6924a7ff40bc8f5e7
jawittdesigns <Killjoy11>
2016-01-13 16:12:52 -0500

    Removed comma

--- template-parts/footer/content-company.php
+++ template-parts/footer/content-company.php
@@ -17,7 +17,7 @@ $olc_phone_number   = fcwp_get_field( 'olc_phone_number', true );
 </div>
 <address class="company__address" itemscope itemtype="http://schema.org/PostalAddress">
   <span itemprop="streetAddress"><?php echo $olc_address; ?>,</span>
-  <span itemprop="addressLocality" class="inline"><?php echo $olc_city; ?></span>, 
+  <span itemprop="addressLocality" class="inline"><?php echo $olc_city; ?></span>
   <span itemprop="addressRegion" class="inline"><?php echo $olc_state; ?></span> 
   <span itemprop="postalCode" class="inline"><?php echo $olc_zip_code; ?></span><br />
   <a href="tel:<?php echo $olc_phone_number; ?>" aria-lable="Phone Number" itemprop="telephone">
