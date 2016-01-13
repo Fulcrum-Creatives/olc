3aad27a19b8d4ff918e6a4296c9e281979137a49
jawittdesigns <Killjoy11>
2016-01-13 12:56:47 -0500

    Added line break to address in the footer

--- template-parts/footer/content-company.php
+++ template-parts/footer/content-company.php
@@ -19,7 +19,7 @@ $olc_phone_number   = fcwp_get_field( 'olc_phone_number', true );
   <span itemprop="streetAddress"><?php echo $olc_address; ?>,</span>
   <span itemprop="addressLocality" class="inline"><?php echo $olc_city; ?></span>, 
   <span itemprop="addressRegion" class="inline"><?php echo $olc_state; ?></span> 
-  <span itemprop="postalCode" class="inline"><?php echo $olc_zip_code; ?></span>
+  <span itemprop="postalCode" class="inline"><?php echo $olc_zip_code; ?></span><br />
   <a href="tel:<?php echo $olc_phone_number; ?>" aria-lable="Phone Number" itemprop="telephone">
     <?php echo $olc_phone_number; ?>
   </a>
