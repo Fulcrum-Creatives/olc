39a23fd4c8f03450b234fbd10a927358a460be6e
jawittdesigns <Killjoy11>
2016-02-22 03:22:17 -0500

    Added Social Icons

--- template-parts/footer/content-company.php
+++ template-parts/footer/content-company.php
@@ -1,20 +1,46 @@
 <?php
-$olc_company_name   = fcwp_get_field( 'olc_company_name', true );
-$olc_footer_tagline = fcwp_get_field( 'olc_footer_tagline', true );
-$olc_address        = fcwp_get_field( 'olc_address', true );
-$olc_apt_suite      = fcwp_get_field( 'olc_apt_suite', true );
-$olc_city           = fcwp_get_field( 'olc_city', true );
-$olc_state          = fcwp_get_field( 'olc_state', true );
-$olc_zip_code       = fcwp_get_field( 'olc_zip_code', true );
-$olc_phone_number   = fcwp_get_field( 'olc_phone_number', true );
+$olc_company_name = fcwp_get_field( 'olc_company_name', true );
+$olc_address      = fcwp_get_field( 'olc_address', true );
+$olc_apt_suite    = fcwp_get_field( 'olc_apt_suite', true );
+$olc_city         = fcwp_get_field( 'olc_city', true );
+$olc_state        = fcwp_get_field( 'olc_state', true );
+$olc_zip_code     = fcwp_get_field( 'olc_zip_code', true );
+$olc_phone_number = fcwp_get_field( 'olc_phone_number', true );
+$olc_facebook     = fcwp_get_field( 'olc_facebook', true );
+$olc_linkedin     = fcwp_get_field( 'olc_linkedin', true );
+$olc_twitter      = fcwp_get_field( 'olc_twitter', true );
 ?>
 <div class="company">
-  <span itemprop="name" class="company__name">
+  <div itemprop="name" class="company__name">
     <?php echo $olc_company_name; ?>
-  </span>
-  <span class="company__text">
-    <?php echo $olc_footer_tagline; ?>
-  </span>
+  </div>
+  <div class="social-Links">
+    <?php if( $olc_facebook ) : ?>
+      <div class="social-links__icon facebook">
+        <a href="<?php echo $olc_facebook; ?>">
+          <i class="fa fa-facebook-square"></i>
+        </a>
+      </div>
+    <?php
+    endif;
+    if( $olc_linkedin ) :
+    ?>
+    <div class="social-links__icon linkedin">
+      <a href="<?php echo $olc_linkedin; ?>">
+        <i class="fa fa-linkedin-square"></i>
+      </a>
+    </div>
+    <?php
+    endif;
+    if( $olc_linkedin ) :
+    ?>
+    <div class="social-links__icon twitter">
+      <a href="<?php echo $olc_twitter; ?>">
+        <i class="fa fa-twitter"></i>
+      </a>
+    </div>
+    <?php endif; ?>
+  </div>
 </div>
 <address class="company__address" itemscope itemtype="http://schema.org/PostalAddress">
   <span itemprop="streetAddress">
